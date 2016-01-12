3794d2da69af979f1f5ef4cfb1d68d66c71bcdc2
Yuri Schimke <yuri@schimke.ee>
2016-01-12 00:13:03 +0000

    oksocial 1.0.0

--- oksocial.rb
+++ oksocial.rb
@@ -1,9 +1,9 @@
 class Oksocial < Formula
   desc "OkSocial"
   homepage "https://github.com/yschimke/oksocial"
-  url "https://oss.sonatype.org/content/repositories/snapshots/com/baulsupp/oksocial/1.0.0-SNAPSHOT/oksocial-1.0.0-20160111.224140-2-bundle.tar.gz"
-  version "1.0.0-SNAPSHOT"
-  sha256 "f550de1cd9cf6f95ab29ceadeeac60eb974ea0d842a8838fb08171cf02ab9aea"
+  url "http://repo1.maven.org/maven2/com/baulsupp/oksocial/1.0.0/oksocial-1.0.0-bundle.tar.gz"
+  version "1.0.0"
+  sha256 "953ed010e51a74ae7946276d43cf93bfb19d5d82b26b355f1947fa97b5a59014"
 
   depends_on :java
 
