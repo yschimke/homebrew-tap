91aea28785c04714d387fbd67c983b271c1a1359
Yuri Schimke <yuri@schimke.ee>
2016-06-25 15:27:46 -0700

    twilio commands

--- oksocial.rb
+++ oksocial.rb
@@ -1,8 +1,8 @@
 class Oksocial < Formula
   desc "OkSocial"
   homepage "https://github.com/yschimke/oksocial"
-  url "https://github.com/yschimke/oksocial/releases/download/oksocial-1.0.12/oksocial-1.0.12-bundle.tar.gz"
-  sha256 "94c37a8cda83caf6d054724ad965bd440da13f5569332a188c4d64203d82eabe"
+  url "https://github.com/yschimke/oksocial/releases/download/oksocial-1.0.13/oksocial-1.0.13-bundle.tar.gz"
+  sha256 "d998f13513bfbb3e4774776f97fccfcd01a9c6980dc00cda49f07956bbb86f05"
 
   depends_on :java
   depends_on "jq" => :recommended
@@ -25,6 +25,9 @@ class Oksocial < Formula
     bin.install_symlink "#{libexec}/bin/squareapi"
     bin.install_symlink "#{libexec}/bin/stackexchangeapi"
     bin.install_symlink "#{libexec}/bin/tweetsearch"
+    bin.install_symlink "#{libexec}/bin/twilioapi"
+    bin.install_symlink "#{libexec}/bin/twiliolist"
+    bin.install_symlink "#{libexec}/bin/twiliosend"
     bin.install_symlink "#{libexec}/bin/twitterapi"
     bin.install_symlink "#{libexec}/bin/uberapi"
   end
