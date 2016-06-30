82dbed6152ee1d8b2e12206bd99e42b0ccf64b9d
Yuri Schimke <yuri@schimke.ee>
2016-06-30 14:07:00 -0700

    prerelease

--- reactivesocket-cli.rb
+++ reactivesocket-cli.rb
@@ -1,14 +1,15 @@
 class ReactivesocketCli < Formula
   desc "ReactiveSocket CLI"
   homepage "https://github.com/yschimke/reactivesocket-cli"
-  url "https://github.com/yschimke/reactivesocket-cli/releases/download/HEAD/reactivesocket-cli-0.0.1.tar"
-  version "0.0.1"
-  sha256 "73a2b7da064344c30c2064cbb270edab6a49c87a7e703712ac70d2d168190bde"
+  url "https://github.com/ReactiveSocket/reactivesocket-cli/releases/download/0.0.1-SNAPSHOT/reactivesocket-cli-0.0.1-SNAPSHOT.tar"
+  version "0.0.1-SNAPSHOT"
+  sha256 "ac4a6ac6d36ebf5535f1a1ddb229f6afda77c4e6bf23b01b063f87acbfe02dc2"
 
   depends_on :java
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/reactivesocket-cli"
+    bash_completion.install "#{libexec}/bash/completion.bash" => "reactivesocket-cli"
   end
 end
