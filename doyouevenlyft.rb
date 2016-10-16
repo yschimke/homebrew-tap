0e523c8c066e01dfac17d391f8433feb8e0f8555
Yuri Schimke <yschimke@fb.com>
2016-10-16 17:43:01 +0100

    fix release

--- doyouevenlyft.rb
+++ doyouevenlyft.rb
@@ -1,8 +1,8 @@
 class Doyouevenlyft < Formula
   desc "Doyouevenlyft"
   homepage "https://github.com/yschimke/doyouevenlyft"
-  url "https://github.com/yschimke/doyouevenlyft/archive/0.1.tar.gz"
-  sha256 "b1e16e718dc385591aef8196b24fceb0029d3133a83b8619f4847aeb4eeefd8b"
+  url "https://github.com/yschimke/doyouevenlyft/archive/0.2.tar.gz"
+  sha256 "aa88341c607c47a65b0ac39909c67fca1c885eb8290e744bc131db1814730ae2"
 
   depends_on "oksocial"
 
