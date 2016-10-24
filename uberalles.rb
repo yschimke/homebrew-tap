5dbdb0a411530d09259bae1018b5cade40bbde11
Yuri Schimke <yschimke@fb.com>
2016-10-24 22:03:57 +0100

    uber release

--- uberalles.rb
+++ uberalles.rb
@@ -1,8 +1,8 @@
 class Uberalles < Formula
   desc "Uberalles"
   homepage "https://github.com/yschimke/uberalles"
-  url "https://github.com/yschimke/uberalles/archive/0.1.tar.gz"
-  sha256 "f4518527d3d66f248697ef8d502333668d3e21f0cbefdc593ee7d66f3decf3b7"
+  url "https://github.com/yschimke/uberalles/archive/0.2.tar.gz"
+  sha256 "dfa9e5404af775d4d63b9dfc5a06a3def506c3bfc124dea245f96175e1f10bbb"
 
   depends_on "oksocial"
 
