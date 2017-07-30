6d412d4f81540c719bb73ac78726c7fabf3ad34a
Yuri Schimke <yschimke@fb.com>
2017-07-30 13:28:26 +0100

    release

--- oksocial.rb
+++ oksocial.rb
@@ -1,8 +1,8 @@
 class Oksocial < Formula
   desc "OkSocial"
   homepage "https://github.com/yschimke/oksocial"
-  url "https://github.com/yschimke/oksocial/releases/download/oksocial-1.0.53/oksocial-1.0.53-bundle.tar.gz"
-  sha256 "712f92a9c7a2b4e96381aa69e6552aab3b5de1b7f8b785cc4b73a957d9a375b2"
+  url "https://github.com/yschimke/oksocial/releases/download/oksocial-1.0.54/oksocial-1.0.54-bundle.tar.gz"
+  sha256 "0a0f7ad5ed75b8b6ff20cd18b5e6c83c970aefbfe22cf2e0e55e67ce2573c3be"
 
   depends_on :java
   depends_on "bash-completion" => :recommended
