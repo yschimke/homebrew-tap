075a92a20c50c34ebf466a9eb1d7c95b737ea07f
Yuri Schimke <yuri@schimke.ee>
2018-07-18 11:13:09 +0100

    rename

--- okurl.rb
+++ okurl.rb
@@ -1,9 +1,9 @@
-class Oksocial < Formula
-  desc "OkSocial"
-  homepage "https://github.com/yschimke/oksocial"
-  version "1.49.0"
-  url "https://github.com/yschimke/oksocial/releases/download/#{version}/oksocial-#{version}.tgz"
-  sha256 "11af7cb1f1d8d549861f2eae84776cc8264bfb1ea16c5d91ece82fcbf15332bc"
+class Okurl < Formula
+  desc "OkUrl"
+  homepage "https://github.com/yschimke/okurl"
+  version "1.50.0"
+  url "https://github.com/yschimke/okurl/releases/download/#{version}/okurl-#{version}.tgz"
+  sha256 "d602a22f7235a61608167f9d8c34919543f72a504dea43e8e31ea57c90c48ac8"
 
   depends_on :java => :optional
   depends_on "bash-completion" => :recommended
@@ -12,28 +12,24 @@ class Oksocial < Formula
   def install
     libexec.install Dir["*"]
     inreplace "#{libexec}/bin/okscript", /^export INSTALLDIR.*/, "export INSTALLDIR=#{libexec}"
-    inreplace "#{libexec}/bin/oksocial", /^export INSTALLDIR.*/, "export INSTALLDIR=#{libexec}"
-    bin.install_symlink "#{libexec}/bin/oksocial" => "okapi"
-    bin.install_symlink "#{libexec}/bin/oksocial" => "okws"
+    inreplace "#{libexec}/bin/okurl", /^export INSTALLDIR.*/, "export INSTALLDIR=#{libexec}"
+    bin.install_symlink "#{libexec}/bin/okurl" => "okapi"
+    bin.install_symlink "#{libexec}/bin/okurl" => "okws"
     bin.install_symlink "#{libexec}/bin/4sqapi"
     bin.install_symlink "#{libexec}/bin/fbapi"
     bin.install_symlink "#{libexec}/bin/githubapi"
     bin.install_symlink "#{libexec}/bin/mapboxapi"
     bin.install_symlink "#{libexec}/bin/okscript"
-    bin.install_symlink "#{libexec}/bin/oksocial"
+    bin.install_symlink "#{libexec}/bin/okurl"
     bin.install_symlink "#{libexec}/bin/sorecent.kts" => "sorecent"
     bin.install_symlink "#{libexec}/bin/twitterapi"
     bin.install_symlink "#{libexec}/bin/tweetsearch.kts" => "tweetsearch"
     bin.install_symlink "#{libexec}/bin/uberprices.kts" => "uberprices"
 
-    bash_completion.install "#{libexec}/bash/completion.bash" => "oksocial"
-
-    unless (var/"oksocial/plugins").exist?
-      (var/"oksocial/plugins").mkpath
-    end
+    bash_completion.install "#{libexec}/bash/completion.bash" => "okurl"
   end
 
   test do
-    system "#{bin}/oksocial" "https://api.twitter.com/robots.txt"
+    system "#{bin}/okurl" "https://api.twitter.com/robots.txt"
   end
 end
