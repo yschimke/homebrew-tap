0eedfa0b27bdd02fd3d6151cddb16c01aa168e59
Yuri Schimke <yuri@schimke.ee>
2018-09-08 22:08:11 +0100

    release

--- okurl.rb
+++ okurl.rb
@@ -1,9 +1,9 @@
 class Okurl < Formula
   desc "OkUrl"
   homepage "https://github.com/yschimke/okurl"
-  version "1.52.0"
+  version "1.55.0"
   url "https://github.com/yschimke/okurl/releases/download/#{version}/okurl-#{version}.tgz"
-  sha256 "6fdea229cf41a46cd08e3a90b06ff12a6bab0fd471a624dea73a677289126d45"
+  sha256 "1041cc11072aa0ce67c3fdd99a05ef3254289440e35eda573d4102ae3fed75ca"
 
   depends_on :java => :optional
   depends_on "bash-completion" => :recommended
@@ -16,6 +16,7 @@ class Okurl < Formula
     bin.install_symlink "#{libexec}/bin/okurl" => "okapi"
     bin.install_symlink "#{libexec}/bin/okurl" => "okws"
     bin.install_symlink "#{libexec}/bin/4sqapi"
+    bin.install_symlink "#{libexec}/bin/cronhub-ping"
     bin.install_symlink "#{libexec}/bin/fbapi"
     bin.install_symlink "#{libexec}/bin/githubapi"
     bin.install_symlink "#{libexec}/bin/mapboxapi"
