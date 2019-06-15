bc8b2fb2afb87adf558d7f4ac6ec6dabfac7fc6c
Yuri Schimke <yuri@schimke.ee>
2019-06-15 18:57:51 +0100

    RSocket cli

--- rsocket-cli.rb
+++ rsocket-cli.rb
@@ -1,15 +1,22 @@
 class RsocketCli < Formula
   desc "RSocket CLI"
   homepage "https://github.com/yschimke/reactivesocket-cli"
-  version "1.14.0"
+  version "1.15.0"
   url "https://github.com/rsocket/rsocket-cli/releases/download/#{version}/rsocket-cli-#{version}.tgz"
-  sha256 "97c304c18a89fcbe59e2d0f8d6767f777ec811529713ca8796c93c41b6cb566d"
+  sha256 "f08579cda893a79eea77799a3ac107338d14c4c570ad4db0e6e5254c5e7030fe"
 
   depends_on :java
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/rsocket-cli"
-    bash_completion.install "#{libexec}/bash/completion.bash" => "rsocket-cli"
+    mkdir prefix/"bash"
+
+    system "#{bin}/rsocket-cli --completionScript > #{prefix}/bash/completion.bash"
+    bash_completion.install "#{prefix}/bash/completion.bash" => "rsocket-cli"
+
+    system "echo 'autoload -U +X bashcompinit && bashcompinit' > #{prefix}/bash/completion.zsh"
+    system "#{bin}/rsocket-cli --completionScript >> #{prefix}/bash/completion.zsh"
+    zsh_completion.install "#{prefix}/bash/completion.zsh" => "_rsocket-cli"
   end
 end
