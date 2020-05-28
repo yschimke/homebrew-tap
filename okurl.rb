c3268097e2c845d52f167e989fcb47a11a120368
Yuri Schimke <yuri@schimke.ee>
2020-05-28 23:20:27 +0100

    release

--- okurl.rb
+++ okurl.rb
@@ -1,9 +1,9 @@
 class Okurl < Formula
   desc "OkUrl"
   homepage "https://github.com/yschimke/okurl"
-  version "1.98.0"
+  version "1.100.0"
   url "https://jitpack.io/com/github/yschimke/okurl/#{version}/okurl-#{version}.tar"
-  sha256 "8cc3702819418ea1770cb4b314ab6776da41362b66778bc7ec9e78b9556c7ac4"
+  sha256 "6fc8471b8e20b49f005673838066dd66e732a91c92c287b531f7de35400c584c"
 
   depends_on :java => :optional
   depends_on "bash-completion" => :recommended
@@ -28,6 +28,7 @@ class Okurl < Formula
     bin.install_symlink "#{libexec}/bin/uberprices.kts" => "uberprices"
 
     bash_completion.install "#{libexec}/bash/completion.bash" => "okurl"
+    zsh_completion.install "#{libexec}/zsh/_okurl" => "_okurl"
   end
 
   test do
