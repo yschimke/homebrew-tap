5bc874d5d0a4f65580e61de7cc0967fa9fb1ab21
Yuri Schimke <yuri@schimke.ee>
2020-06-20 13:19:52 +0100

    Releases

--- rsocket-cli.rb
+++ rsocket-cli.rb
@@ -1,9 +1,9 @@
 class RsocketCli < Formula
   desc "RSocket CLI"
   homepage "https://github.com/yschimke/reactivesocket-cli"
-  version "1.23"
+  version "1.25"
   url "https://jitpack.io/com/github/rsocket/rsocket-cli/#{version}/rsocket-cli-#{version}.tar"
-  sha256 "18747149ea61e3a7a957311401186d445d08b05eb12702e82fd399e9829f0f26"
+  sha256 "cb384bd5ded884df7f8287f8f1a4e34461f71c09593bf83ea7dae7d7ebd0c744"
 
   depends_on :java
 
