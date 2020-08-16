6f52b01bf566566d37011b35c57efad4a03d2840
Yuri Schimke <yuri@schimke.ee>
2020-08-16 12:55:18 +0100

    release

--- okurl.rb
+++ okurl.rb
@@ -1,9 +1,9 @@
 class Okurl < Formula
   desc "OkUrl"
   homepage "https://github.com/yschimke/okurl"
-  version "2.15"
+  version "2.16"
   url "https://github.com/yschimke/okurl/releases/download/#{version}/okurl-graal-#{version}.tar"
-  sha256 "f8df4d3a593b3c70d991706e2c1e4a2283d2179e1c0a50f50b9f8d0281ae736d"
+  sha256 "6ccb9df104001b8a390d74b141cdeb1f7532d2ee5fb2e09588a0691f1ee3f5ac"
 
   depends_on "jq" => :recommended
 
