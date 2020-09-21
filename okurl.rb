275e9db765928b74a0c2ee6ab24011ee7a557a5b
Yuri Schimke <yuri@schimke.ee>
2020-09-21 20:27:02 +0100

    Fix

--- okurl.rb
+++ okurl.rb
@@ -1,9 +1,9 @@
 class Okurl < Formula
   desc "OkUrl"
   homepage "https://github.com/yschimke/okurl"
-  version "2.19"
+  version "2.24"
   url "https://github.com/yschimke/okurl/releases/download/#{version}/okurl-graal-#{version}.tar"
-  sha256 "0810bc469a33022dbb8d167f9c1a990095b8e217eb7e04b401fc40c78b7d203d"
+  sha256 "267a48952898cbc748a7643665dd1c42f4b80875f4afbb5cccfbfd208f092585"
 
   depends_on "jq" => :recommended
 
