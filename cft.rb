1163c5432bc4ef508ac5985038c4238ae2f3ea9f
Yuri Schimke <yuri@schimke.ee>
2020-12-03 08:18:55 +0000

    Update cft.rb

--- cft.rb
+++ cft.rb
@@ -6,7 +6,11 @@ class Cft < Formula
   desc "Certifikit CLI"
   homepage "https://github.com/cashapp/certifikit"
   version "0.2.1"
-  url "https://github.com/yschimke/certifikit/releases/download/0.2.1.ys/cft-linux.tar"
+  url "https://github.com/yschimke/certifikit/releases/download/0.2.1.ys/cft-mac.tar"
+  
+  on_linux do
+    url "https://github.com/yschimke/cooee-cli/releases/download/#{version}/cooee-cli-graal-linux-#{version}.tar"
+  end
 
   def install
     libexec.install Dir["*"]
