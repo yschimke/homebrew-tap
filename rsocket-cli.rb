caadfc180cf982065740c29c63d2462e5d9bb227
Yuri Schimke <yuri@schimke.ee>
2021-02-21 11:42:54 +0000

    Graal build

--- rsocket-cli.rb
+++ rsocket-cli.rb
@@ -1,13 +1,13 @@
 class RsocketCli < Formula
   desc "RSocket CLI"
   homepage "https://github.com/yschimke/reactivesocket-cli"
-  version "1.26"
-  url "https://jitpack.io/com/github/rsocket/rsocket-cli/#{version}/rsocket-cli-#{version}.tar"
-  sha256 "d0d6249a59a32b77e3076e9d47d2ec962e4448ea12f92694583a440e1322a532"
+  version "1.29"
+  url "https://github.com/rsocket/rsocket-cli/releases/download/#{version}/rsocket-cli-graal-#{version}.tar"
+  sha256 "4a20ff3fd081c17839356747fe05bbce0428c4307a039e1e5eed83b4aa02df55"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/rsocket-cli"
+    bin.install_symlink "#{libexec}/bin/rsocketcli"
     mkdir prefix/"bash"
 
     zsh_completion.install "#{libexec}/zsh/_rsocket-cli" => "_rsocket-cli"
