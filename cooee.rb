a08f83a75f7e9ab44113c96617df673d1817762e
Yuri Schimke <yuri@schimke.ee>
2021-03-27 09:32:34 +0000

    Release

--- cooee.rb
+++ cooee.rb
@@ -1,12 +1,12 @@
 class Cooee < Formula
   desc "Cooee"
   homepage "https://github.com/yschimke/cooee-cli"
-  version "0.2"
+  version "0.3"
   url "https://github.com/yschimke/cooee-cli/releases/download/#{version}/cooee-cli-graal-#{version}.tar"
   sha256 ""
 
   on_linux do
-    url "https://github.com/yschimke/cooee-cli/releases/download/#{version}/cooee-cli-graal-linux-#{version}.tar"
+    url "https://github.com/yschimke/cooee-cli/releases/download/0.2/cooee-cli-graal-linux-0.2.tar"
     sha256 "9688a34de261588a6ed6b8f99230a6268e2b9f3c17c0a741f4bf79968e005769"
   end
 
