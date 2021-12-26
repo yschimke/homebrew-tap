eb24c3cd6f2cba94ffcb6c92e9484746380d1bd9
jreleaserbot <jreleaser@kordamp.org>
2021-12-26 19:00:59 +0000

    okurl v3.11.0.0

--- Formula/okurl.rb
+++ Formula/okurl.rb
@@ -2,8 +2,8 @@
 class Okurl < Formula
   desc "OkHttp Kotlin command line"
   homepage "https://github.com/yschimke/okurl"
-  url "https://github.com/yschimke/okurl/releases/download/v3.9.0/okurl-graal-3.9.0.zip"
-  version "3.9.0"
+  url "https://github.com/yschimke/okurl/releases/download/v3.11.0.0/okurl-graal-3.11.0.0.zip"
+  version "3.11.0.0"
   sha256 "0a45ff3a9f9936482ded01bc1d5fbbc1b0e77cb064f518ad2cca430c34d81abc"
   license "Apache-2.0"
 
@@ -18,6 +18,6 @@ class Okurl < Formula
 
   test do
     output = shell_output("#{bin}/okurl --version")
-    assert_match "3.9.0", output
+    assert_match "3.11.0.0", output
   end
 end
