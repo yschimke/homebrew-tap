371766ee8b998a3358a7c2be6c83e2044f2ab49d
jreleaserbot <jreleaser@kordamp.org>
2021-12-29 21:34:54 +0000

    emulator-tools v0.4.3.0

--- Formula/emulator-tools.rb
+++ Formula/emulator-tools.rb
@@ -1,9 +1,10 @@
+# Generated with JReleaser 0.10.0 at 2021-12-29T21:34:43.944756Z
 class EmulatorTools < Formula
   desc "Android Emulator Tools"
   homepage "https://github.com/yschimke/emulator-tools"
-  url "https://github.com/yschimke/emulator-tools/releases/download/v0.4.0.0/emulator-tools-graal-0.4.0.0.zip"
-  version "0.4.0.0"
-  sha256 "4b98d5afb851aff89db746908749cde3bd2eaa839e1de3a4470daa63eeee35a0"
+  url "https://github.com/yschimke/emulator-tools/releases/download/v0.4.3.0/emulator-tools-0.4.3.0-osx-x86_64.zip"
+  version "0.4.3.0"
+  sha256 "eca10a175e0810b8e3c844f31842093ec8bb228d843083c040f7e87c1ecf04ff"
   license "Apache-2.0"
 
 
@@ -14,6 +15,6 @@ class EmulatorTools < Formula
 
   test do
     output = shell_output("#{bin}/emulator-tools --version")
-    assert_match "0.4.0.0", output
+    assert_match "0.4.3.0", output
   end
 end
